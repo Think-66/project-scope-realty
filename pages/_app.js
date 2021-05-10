170ebb337936423cd3f94a5b9bb09f71c625fcd2
devyio <37695392+devyio@users.noreply.github.com>
2021-05-10 20:49:35 +0530

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../public/css/main.css'
+import '../public/css/bootstrap.min.css'
+import '../public/css/style.css'
+import '../public/css/jqvmap.css'
+
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
