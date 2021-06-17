2bbd5966dcf3adfc280a12698ce2b93b1f7f25d5
devyio <37695392+devyio@users.noreply.github.com>
2021-06-17 18:26:08 +0530

    Add new components folder

---
+++ components/ProtectedLayout.js
@@ -0,0 +1,31 @@
+import Header from "./header";
+import Footer from "./footer";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/client";
+import Layout from "./layout";
+import AccessDenied from "./access-denied";
+import { ToastContainer } from "react-nextjs-toast";
+
+export default function ProtectedLayout({ children }) {
+  const [session, loading] = useSession();
+
+  // When rendering client side don't display anything until loading is complete
+  if (typeof window !== "undefined" && loading) return null;
+
+  // If no session exists, display access denied message
+  if (!session) {
+    return (
+      <Layout>
+        <AccessDenied />
+      </Layout>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
