a8aff3fc346c5b7fc7cdac2ef2030186fe601901
devyio <37695392+devyio@users.noreply.github.com>
2021-07-10 00:47:14 +0530

    Change http to https
    Change http to https

--- services/listingApiService.js
+++ services/listingApiService.js
@@ -1,9 +1,9 @@
 import { getRequest } from './coreApiServices'
 
 export const getListingsDataa = () => {
-    return getRequest(`http://dataapi.realtymx.com/listings?apiKey=487133496c474d70`)
+    return getRequest(`https://dataapi.realtymx.com/listings?apiKey=487133496c474d70`)
 }
 
 export const getListingsRealtym = () => {
-    return getRequest(`http://api.realtymx.com/properties/?apikey=4e6d6261566c3179`)
+    return getRequest(`https://api.realtymx.com/properties/?apikey=4e6d6261566c3179`)
 }
