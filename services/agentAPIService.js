f035065c84d6cad136cdeaa68dcd35f5b6d315df
devyio <37695392+devyio@users.noreply.github.com>
2021-07-15 18:48:39 +0530

    Change the http to https

--- services/agentAPIService.js
+++ services/agentAPIService.js
@@ -2,12 +2,12 @@ import { getRequest } from "./coreApiServices";
 
 export const getAgents = () => {
   return getRequest(
-    `http://api.realtymx.com/agents/search/?apikey=4e6d6261566c3179`
+    `https://api.realtymx.com/agents/search/?apikey=4e6d6261566c3179`
   );
 };
 
 export const getAgentById = (id) => {
   return getRequest(
-    `http://api.realtymx.com/agents/` + id + `/?apikey=4e6d6261566c3179`
+    `https://api.realtymx.com/agents/` + id + `/?apikey=4e6d6261566c3179`
   );
 };
