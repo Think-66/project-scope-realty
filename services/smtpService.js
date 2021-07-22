17d2cd76ffb14ca7bd92c515205895273d70ddfb
devyio <37695392+devyio@users.noreply.github.com>
2021-07-22 23:25:49 +0530

    Smtp service implementation

---
+++ services/smtpService.js
@@ -0,0 +1,32 @@
+export default function (req, res) {
+    require('dotenv').config()
+
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+      port: 465,     
+      host: "smtp.gmail.com",
+         auth: {
+              user: 'chapman1lucy1@gmail.com',
+              pass: "Lucy@2021",
+           },
+      secure: true,
+    });
+    
+    const mailData = {
+        from: 'chapman1lucy1@gmail.com',
+        to: 'kanishkashm@gmail.com',
+        subject: `Message From ${req.body.name}`,
+        text: req.body.message + " | Sent from: " + req.body.email,
+        html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+    }
+  
+    transporter.sendMail(mailData, function (err, info) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info);
+    })
+  
+    console.log(req.body)
+    res.send('success')
+  }
